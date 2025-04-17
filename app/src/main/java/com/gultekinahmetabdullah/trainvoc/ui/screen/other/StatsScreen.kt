d82caa7989d20bb12c65c188d941a80a4b8c1ee8
Ahmet Abdullah Gültekin <ahmetabdullahgultekin@gmail.com>
2025-04-17 16:05:09 +0100

    README added. Directories managed.

--- app/src/main/java/com/gultekinahmetabdullah/trainvoc/ui/screen/other/StatsScreen.kt
+++ app/src/main/java/com/gultekinahmetabdullah/trainvoc/ui/screen/other/StatsScreen.kt
@@ -1,4 +1,4 @@
-package com.gultekinahmetabdullah.trainvoc.ui.screen.extra
+package com.gultekinahmetabdullah.trainvoc.ui.screen.other
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
